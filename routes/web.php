85147e940397ef7a0efdd1dbcc479da3b683a4d3
ManganiBiagio <biagiomangani@live.it>
2023-01-26 14:49:11 +0100

    add comics index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicsController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class,"index"]);
 
+Route::get("/comics",[ComicsController::class,"index"])->name("comics.index");
