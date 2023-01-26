426c9b81b3e7ed50aa89afc2b7b236a1e5beab87
ManganiBiagio <biagiomangani@live.it>
2023-01-26 14:51:19 +0100

    add correct link in header for comic index

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <nav>
         <ul class="list2 d-flex list-unstyled gap-3 align-items-center m-0">
           <li><a href="/" >Home</a></li>
-          <li><a href="#">Comics</a></li>
+          <li><a href="{{route("comics.index")}}">Comics</a></li>
           
         </ul>
       </nav>
