c327576e650c21d61b947a314167457543fe1931
ManganiBiagio <biagiomangani@live.it>
2023-01-30 16:34:31 +0100

    improve comic shoe

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,8 +2,25 @@
 @section("content")
 @section('content')
 <div class="container">
-    <h1>{{$comic->title}}</h1>
-    <p>{{$comic->description}}</p>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">
+                <img src="{{$comic["thumb"]}}" style="width: 30px" alt="">
+                {{$comic["title"]}}
+            </h5>
+            <p class="card-text"><strong>Descrizione:</strong>{{$comic["description"]}}</p>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>Prezzo:</strong>{{$comic["price"]}}$</li>
+                <li class="list-group-item"><strong>Serie:</strong>{{$comic["series"]}}</li>
+                <li class="list-group-item"><strong>Tipologia:</strong>{{$comic["type"]}}</li>
+              </ul>
+
+            
+           
+            
+            
+        </div>
+    </div>
 </div>
   
   
